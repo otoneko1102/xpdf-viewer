bca36b2de6bc78bbc63ad92ff4d0ea288d63ead3
otoneko1102 <pineapple2006.11.2@gmail.com>
2025-08-31 10:25:31 +0900

    README.mdにパッケージ名のプレースホルダーを追加し、バージョン情報と共に更新するように修正

--- dev/replace.js
+++ dev/replace.js
@@ -2,10 +2,11 @@ const fs = require("fs-extra");
 const path = require("path");
 
 const pkg = fs.readJsonSync(path.join(__dirname, "../package.json"));
+const name = pkg.name;
 const version = pkg.version;
 
 let README = fs.readFileSync(path.join(__dirname, "./README.md"), "utf-8");
-README = README.replace(/%version%/g, version);
+README = README.replace(/%name%/g, name).replace(/%version%/g, version);
 
 fs.writeFileSync(path.join(__dirname, "../README.md"), README, "utf-8");
 
